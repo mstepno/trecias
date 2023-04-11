6f4b76660da9f291caa2e898095bb208611f04ce
mstepno <121028153+mstepno@users.noreply.github.com>
2023-04-11 22:30:18 +0300

    Trecias

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Barsukas',
+            'email' => 'barsukas@gmail.com',
+            'password' => Hash::make('abc123'),
+        ]);
+        
+        $faker = Faker::create('lt_LT');
+
+        foreach(range(1, 5) as $_) {
+            DB::table('clients')->insert([
+                'name' => $faker->firstName,
+                'surname' => $faker->lastName,
+                'pid' => rand(10000000000, 99999999999),
+                'iban' => 'LT'.rand(100000000000000000, 999999999999999999),
+                'funds' => (float) rand(0, 1000000).'.'.rand(0, 99),
+            ]);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController as CL;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [CL::class, 'index'])->name('index');
+
+Route::prefix('bank')->name('clients-')->group(function () {
+    Route::get('/', [CL::class, 'index'])->name('index');
+    Route::get('/create', [CL::class, 'create'])->name('create');
+    Route::post('/create', [CL::class, 'store'])->name('store');
+    Route::get('/{client}', [CL::class, 'show'])->name('show');
+    Route::get('/edit/{client}', [CL::class, 'edit'])->name('edit');
+    Route::put('/edit/{client}', [CL::class, 'update'])->name('update');
+    Route::delete('/delete/{client}', [CL::class, 'destroy'])->name('delete');
+    Route::get('/add/{client}', [CL::class, 'add'])->name('add');
+    Route::put('/add/{client}', [CL::class, 'addUpdate'])->name('addUpdate');
+    Route::get('/deduct/{client}', [CL::class, 'withdraw'])->name('deduct');
+    Route::put('/deduct/{client}', [CL::class, 'withdrawUpdate'])->name('withdrawUpdate');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [CL::class, 'index'])->name('home');
